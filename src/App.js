0356134141f9955dcc645da765d26f9a135016ce
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-19 13:48:28 +0100

    adding the paragraph dynamically

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const messages = [
 ];
 
 export default function App() {
+  const step = 1;
   return (
     <div
       className="steps
@@ -15,7 +16,9 @@ export default function App() {
         <div>2</div>
         <div>3</div>
       </div>
-      <p className="message"></p>
+      <p className="message">
+        Step {step} : {messages[step - 1]}
+      </p>
       <div className="buttons">
         <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
           Previous
