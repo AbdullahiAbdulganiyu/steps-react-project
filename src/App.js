35d434f39bf28a285dc3b253627b5ab0bafef2f9
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-19 13:40:22 +0100

    making the first element active

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     "
     >
       <div className="numbers">
-        <div>1</div>
+        <div className="active">1</div>
         <div>2</div>
         <div>3</div>
       </div>
